cc9e003db63e285d61f54387caf686e601038f8b
cmv534 <cmv534@alumni.ku.dk>
2024-04-07 14:21:18 +0200

    Improving Inuagural Project

--- Other/Old ExchangeEconomy.py
+++ Other/Old ExchangeEconomy.py
@@ -20,18 +20,18 @@ class ExchangeEconomyClass:
     def utility_B(self,x1B,x2B):
         return x1B**self.par.beta * x2B**(1-self.par.beta)
 
-    def demand_A(self, p1, p2=1):
+    def demand_A(self,p1,p2=1):
         par = self.par
         I = p1*par.w1A + p2*par.w2A  # Total income for A
-        x1A_star = min(max(par.alpha * I / p1, 0), 1)  # Ensure x1A is between 0 and 1
-        x2A_star = min(max((1 - par.alpha) * I / p2, 0), 1)  # Ensure x2A is between 0 and 1
+        x1A_star = par.alpha * I / p1
+        x2A_star = (1 - par.alpha) * I / p2
         return x1A_star, x2A_star
 
     def demand_B(self,p1,p2=1):
         par = self.par
         I = p1*(1 - par.w1A) + p2*(1 - par.w2A)  # Total income for B
-        x1B_star = min(max(par.beta * I / p1, 0), 1) # Ensure x1B is between 0 and 1
-        x2B_star = min(max((1 - par.beta) * I / p2, 0), 1) # Ensure x2B is between 0 and 1
+        x1B_star = par.beta * I / p1
+        x2B_star = (1 - par.beta) * I / p2
         return x1B_star, x2B_star
 
     def check_market_clearing(self,p1,p2=1):
